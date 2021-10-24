e452dfe0eec91e4de36c9bc8c24ff4a709a9a1ad
manuel41 <medmundogonzalez@gmail.com>
2021-10-23 23:16:18 -0400

    Remove container class

--- src/useStyles.js
+++ src/useStyles.js
@@ -1,10 +1,6 @@
 import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme) => ({
-    container: {
-        padding: theme.spacing(8, 0, 6),
-        backgroundColor: 'lightgray'
-    },
     app: {
         textAlign: 'center',
         marginTop: '70px'
