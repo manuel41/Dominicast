dc6dfe4857287b5dfb9cfe364e13115a87243c31
Enmanuel Minaya <enmanuelminaya@gmail.com>
2021-10-24 21:13:52 -0400

    appbar design

--- src/theme.jsx
+++ src/theme.jsx
@@ -2,6 +2,16 @@ import { createTheme } from '@mui/material/styles'
 const theme = createTheme({
     typography: {
         fontFamily: ['"Arima Madurai"', 'Open Sans'].join(',')
+    },
+    palette: {
+        primary: { main: '#003166' },
+        secondary: {
+            main: '#792836',
+            lighter: '#792836',
+        },
+        neutral: {
+            main: '#D8D8D8',
+        },
     }
 })
 
