0274eae4b6ef33c04d0366df49ddb40f8b7f35f2
Manuel Gonzalez <49133037+manuel41@users.noreply.github.com>
2021-11-30 23:30:21 -0400

    Moved UserProvider to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Discover from './Discover';
+import UserProvider from './components/UserContext';
 import Home from './Home';
 import ProfileView from './ProfileView'
 import Layout from './components/Layout';
@@ -19,7 +20,9 @@ const App = () => {
             <Route exact path="/" component={Home} />
             <Route path="/discover" component={Discover} />
             <Route path="/profile/:id" children={<ProfileView />} />
-            <Route path="/create-profile" component={CreateProfile} />
+            <UserProvider>
+              <Route path="/create-profile" component={CreateProfile} />
+            </UserProvider>
           </Switch>
         </Layout>
       </Router>
