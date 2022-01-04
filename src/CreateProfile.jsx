8dc8db0e2536f200a50cad218f980fab94fe1a39
Manuel Gonzalez <medmundogonzalez@gmail.com>
2022-01-04 19:19:29 -0400

    Changes to create profile view

--- src/CreateProfile.jsx
+++ src/CreateProfile.jsx
@@ -28,7 +28,7 @@ import { useUserUpdate } from "./components/UserContext";
 //   );
 // }
 
-const steps = ['Registrar Usuario', 'Crear Perfil', 'Detalles Perfil'];
+const steps = ['Detalles Usuario', 'Perfil', 'Destrezas'];
 
 function getStepContent(step) {
   switch (step) {
@@ -45,14 +45,15 @@ function getStepContent(step) {
 
 const CreateProfile = () => {
   // const classes = useStyles();
-  const { registerNewUser } = useUserUpdate()
+  const { registerNewUser, updateUser } = useUserUpdate()
   const [activeStep, setActiveStep] = useState(0);
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
     console.log(activeStep);
     if (activeStep === 2) {
-      registerNewUser()
+      // registerNewUser();
+      updateUser();
     }
   };
 
@@ -62,15 +63,6 @@ const CreateProfile = () => {
 
   return (
     <>
-      {/* <Container>
-        <Paper className={classes.paperRoot}>
-          <Grid container className={classes.mainGrid}>
-            <Grid item xs={12} sm={6}>
-
-            </Grid>
-          </Grid>
-        </Paper>
-      </Container> */}
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
@@ -87,7 +79,7 @@ const CreateProfile = () => {
             {activeStep === steps.length ? (
               <>
                 <Typography variant="h5" gutterBottom>
-                  Usuario Registrado!
+                  Accion realizada con exito!
                 </Typography>
               </>
             ) : (
@@ -105,7 +97,7 @@ const CreateProfile = () => {
                     onClick={handleNext}
                     sx={{ mt: 3, ml: 1 }}
                   >
-                    {activeStep === steps.length - 1 ? 'Registrar' : 'Siguiente'}
+                    {activeStep === steps.length - 1 ? 'Finalizar' : 'Siguiente'}
                   </Button>
                 </Box>
               </>
